2251dfe6fa3fe622b7bb234ed77e695ffea52ec8
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 08:54:27 +0530

    Update zoro_bot.py

--- zoro_bot.py
+++ zoro_bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = "19099900"
+API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
+BOT_TOKEN = "6206599982:AAGELlIUapiHd88l5z4YuVwXp1h3tHMfotY"
 
 app = Client("zoro_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
