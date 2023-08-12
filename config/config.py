126826f89b178e87fbdfd6faed2f30b2ab7563e2
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 09:11:36 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,22 @@
-# API credentials
-API_ID = "19099900"
-API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
-BOT_TOKEN = "6206599982:AAGELlIUapiHd88l5z4YuVwXp1h3tHMfotY"
+API_ID = 14681826  # Your API ID
+API_HASH = "add59ab14dbbccf3c92c65ca4477f2fa"  # Your API Hash
+BOT_TOKEN = "e738a41537msh518a25cf253209fp13958fjsn07ed13e97c48"  # Your bot token
 
-# Database configuration
-MONGO_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority/"
-DB_NAME = "zoro_bot_db"
+ERROR_ID = -1001905486162  # private -- Blue Errors log
+SUPPORT_CHAT = "NanoSTestingArea"
+SUPPORT_ID = -1001905486162  # devslab
+
+DOWNLOAD_DIRECTORY = "./"
+
+MONGO_DB = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority"
+
+OWNER_ID = [6198858059]
+
+SUDOLIST = []  # REPORTERS
+SUPPORTLIST = []  # Inspectors
+DEV_LIST = []
+DEV_USERS = DEV_LIST + OWNER_ID
+SUDO_USERS = SUDOLIST + DEV_USERS  # Enforcers
+SUPPORT_USERS = SUPPORTLIST + SUDO_USERS  # Inspectors
+
+# Other constants and configurations can be added here as needed
