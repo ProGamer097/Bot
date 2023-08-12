f4a0072848fc321fa4d08d9817a0990c15c36800
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 08:49:17 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,8 +1,8 @@
 # API credentials
-API_ID = "your_api_id"
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = "19099900"
+API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
+BOT_TOKEN = "6206599982:AAGELlIUapiHd88l5z4YuVwXp1h3tHMfotY"
 
 # Database configuration
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://sonu55:sonu55@cluster0.vqztrvk.mongodb.net/?retryWrites=true&w=majority/"
 DB_NAME = "zoro_bot_db"
