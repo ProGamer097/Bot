4d1580e7984f81bcea9d1c9a1298e3b912e95ee3
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 08:36:57 +0530

    Update bounty.py

--- commands/bounty.py
+++ commands/bounty.py
@@ -6,9 +6,11 @@ bounties_collection = db["bounties"]
 
 @Client.on_message(filters.command("bounty"))
 async def bounty_command(client, message):
+    user_id = message.from_user.id
+    add_user(user_id)  # Add user to database
+    
     try:
         text = message.text.split(None, 1)[1]
-        user_id = message.from_user.id
         amount = int(text)
     except:
         return await message.reply_text("Format:\n`/bounty (amount)`")
@@ -17,19 +19,14 @@ async def bounty_command(client, message):
         return await message.reply_text("Amount must be greater than 0.")
     
     # Update or insert user's bounty amount
-    bounties_collection.update_one({"user_id": user_id}, {"$inc": {"amount": amount}}, upsert=True)
+    update_bounty(user_id, amount)
     
     await message.reply_text(f"You've added {amount} to your bounty!")
 
 @Client.on_message(filters.command("mybounty"))
 async def my_bounty_command(client, message):
     user_id = message.from_user.id
-    bounty = bounties_collection.find_one({"user_id": user_id})
+    add_user(user_id)  # Add user to database
     
-    if not bounty:
-        await message.reply_text("You don't have a bounty yet.")
-    else:
-        await message.reply_text(f"Your bounty: {bounty['amount']}")
-
-# Implement other bounty-related commands if needed
-
+    bounty = get_bounty(user_id)
+    await message.reply_text(f"Your bounty: {bounty}")
