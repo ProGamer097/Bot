0b93ddf54aab0acbe58791d8e07949cf667828b8
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 08:33:00 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -7,10 +7,11 @@ db = mongo_client[DB_NAME]
 
 # Define collections
 characters_collection = db["characters"]
-# Define other collections as needed
+bounties_collection = db["bounties"]
 
 # Sample implementation of database functions
 
+# Characters
 def upload_character(url, char_id, anime_id, name):
     character_data = {
         "char_id": char_id,
@@ -26,6 +27,12 @@ def get_character_by_id(char_id):
 def get_all_characters():
     return list(characters_collection.find())
 
-# Implement other database functions here
-# For example, functions to update, delete, or perform other operations
+# Bounties
+def update_bounty(user_id, amount):
+    bounties_collection.update_one({"user_id": user_id}, {"$inc": {"amount": amount}}, upsert=True)
 
+def get_bounty(user_id):
+    bounty = bounties_collection.find_one({"user_id": user_id})
+    return bounty['amount'] if bounty else 0
+
+# Implement other database functions here
