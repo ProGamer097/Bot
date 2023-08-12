e84a7df6d07d2f6c34338c619a899e337579803f
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-08-12 08:22:31 +0530

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,8 @@
+# API credentials
+API_ID = "your_api_id"
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+
+# Database configuration
+MONGO_URI = "mongodb://localhost:27017/"
+DB_NAME = "zoro_bot_db"
