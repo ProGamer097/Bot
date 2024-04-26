16c5232df1d909806734b384a62a67ace8081c5d
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-04-26 11:26:59 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,34 +1,20 @@
 import logging
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-
-# Import commands from each module
 from commands import start, bounty, upload
 from database import init
 
-# Configure logging
 logging.basicConfig(level=logging.INFO)
-
-# Create the Pyrogram client
 app = Client("zoro_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 if __name__ == "__main__":
     try:
-        # Load modules
-        init()  # Initialize the database
-        
-        # Load command modules
+        init()  
         start.load(app)
         bounty.load(app)
         upload.load(app)      
-        
-        # Run the Pyrogram client
         app.start()
-
-        # Stop the Pyrogram client on program termination
         app.stop()
-        
-        # Send a deployment successful message
         with app:
             app.send_message("-1001905486162", "Zoro Bot has been deployed successfully!")
     except Exception as e:
